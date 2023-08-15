b2d4a7d7dc0f872445eafb579ab1ab7b1dd73c73
Poviraevsergei <poviraevsergei@gmail.com>
2023-08-15 21:39:28 +0300

    CRUD Hibernate Query

--- src/main/java/com/tms/repository/UserRepository.java
+++ src/main/java/com/tms/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.tms.repository;
 
 import com.tms.domain.UserInfo;
+import jakarta.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -19,9 +20,16 @@ public class UserRepository {
 
     //Read
     public UserInfo findById(int id) {
+        UserInfo userInfo = null;
         Session session = sessionFactory.openSession();
-        UserInfo userInfo = session.find(UserInfo.class, id);
+        Query<UserInfo> query = session.createQuery("FROM user_info u WHERE u.id =: userId", UserInfo.class);
+        query.setParameter("userId", id);
+        List<UserInfo> resultList = query.getResultList();
         session.close();
+
+        if (resultList != null && !resultList.isEmpty()) {
+            userInfo = resultList.get(0);
+        }
         return userInfo;
     }
 
@@ -36,6 +44,7 @@ public class UserRepository {
 
     //Create
     public void save(UserInfo userInfo) {
+        //не можем использовать Query для сохранения, только если перенести из другой таблицы
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.persist(userInfo);
@@ -46,8 +55,16 @@ public class UserRepository {
     //Update
     public void updateUser(UserInfo userInfo) {
         Session session = sessionFactory.openSession();
+        Query<UserInfo> query = session.createQuery("UPDATE user_info SET firstName=:firstName, " +
+                "lastName=:lastName, updatedAt=:updatedAt, role=:role WHERE id=:id");
+        query.setParameter("id", userInfo.getId());
+        query.setParameter("firstName", userInfo.getFirstName());
+        query.setParameter("lastName", userInfo.getLastName());
+        query.setParameter("updatedAt", userInfo.getUpdatedAt());
+        query.setParameter("role", userInfo.getRole());
+
         session.beginTransaction();
-        session.merge(userInfo);
+        query.executeUpdate();
         session.getTransaction().commit();
         session.close();
     }
@@ -55,8 +72,11 @@ public class UserRepository {
     //Delete
     public void delete(UserInfo userInfo) {
         Session session = sessionFactory.openSession();
+        Query<UserInfo> query = session.createQuery("DELETE user_info WHERE id=:id");
+        query.setParameter("id", userInfo.getId());
+
         session.beginTransaction();
-        session.remove(userInfo);
+        query.executeUpdate();
         session.getTransaction().commit();
         session.close();
     }
